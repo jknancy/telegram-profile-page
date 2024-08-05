c27223877905e998f6d98baea31bd446f06086e1
jknancy <nancy@mail.sb>
2024-08-05 10:55:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$slug="home";
+require_once('core/header.php');?>
+
+<h1>NANCY</h1>
+<h2>Telegram Style Personal Profile Page</h2>
+
+<p>Some Text Here...</p>
+
+    <?php
+    require_once('core/footer.php');?>
